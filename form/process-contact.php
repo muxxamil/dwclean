860fbfe2daf95c2d6c5589b04057e3d9ab5d0642
118414 <u90839501@cbi-bs1.server.lan>
2018-06-18 15:15:31 -0400

    email form setting

--- form/process-contact.php
+++ form/process-contact.php
@@ -1,22 +1,28 @@
 <?php
 
-$to = "";  // Your email here
+$to = "m.muzamilejaz@gmail.com";  // Your email here
 $from = $_REQUEST['email'];
 $name = $_REQUEST['name'];
 //$phone = $_REQUEST['phone'];
 $message = $_REQUEST['message'];
-$headers = "From: $from";
+$headers = "From: $from \r\n";
+
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
 $subject = "Contact Form from Cleaning Service";
 
 $fields = array();
-$fields{"name"} = "First name";
+$fields{"name"} = "Name";
 $fields{"email"} = "Email";
 //$fields{"phone"} = "Phone";
 $fields{"message"} = "Message";
 
-$body = "Here is what was sent:\n\n";
+$body = "Here is what was sent: <br/><br/>";
 foreach($fields as $a => $b){   
-	$body .= sprintf("%20s:%s\r\n",$b,$_REQUEST[$a]);
+	//$body .= sprintf("%20s:%s\r\n",$b,$_REQUEST[$a]);
+	$tempStr = "<b>" . $b . ":</b> " . $_REQUEST[$a] . "<br/><br/>";
+	$body .= $tempStr;
 }
 $send = mail($to, $subject, $body, $headers);
 
